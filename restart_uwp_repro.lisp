df2ebe4095346f601d0dd33671de5de49e9dc84a
Manfred Bergmann <manfred@nnamgreb.de>
2026-06-02 19:56:33 +0200

    new boot.fasl

--- restart_uwp_repro.lisp
+++
@@ -1,8 +0,0 @@
-(defvar *log* nil)
-(let ((*log* nil))
-  (restart-case
-    (unwind-protect
-      (progn (push 'before *log*) (invoke-restart 'abort))
-      (push 'cleanup *log*))
-    (abort () (push 'handler *log*) :done))
-  (format t "Order: ~S~%" (reverse *log*)))
